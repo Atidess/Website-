334654b08b2ecab8eeaf60edc2ae9269fd8d9701
Atidess <157421947+Atidess@users.noreply.github.com>
2026-08-15 04:03:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-app.post("/rank", (req, res) => {
-    console.log("🔥 /rank WURDE AUFGERUFEN!");
-    console.log("Daten:", req.body);
-
-    res.json({
-        success: true,
-        message: "Rank API funktioniert!"
-    });
-});
-
 const express = require("express");
 
 const app = express();
